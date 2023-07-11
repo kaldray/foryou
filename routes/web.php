328371b05413f02c83da4f61074c5fe5e9d1c2e3
kaldray <jordray@protonmail.com>
2023-07-11 08:55:38 +0200

    test:redirect

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::prefix("/")
   ->name("guest.")
   ->group(function () {
+    Route::get("/auth/redirect", function () {
+      return Socialite::driver("tiktok")->redirect();
+    });
+    Route::get("/auth/callback", function () {
+      $user = Socialite::driver("tiktok")->user();
+      dd($user);
+    });
     Route::get("/", function () {
       return view("base");
     });
